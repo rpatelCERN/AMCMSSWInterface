70818f1761d07392dda183c6b44a2231e1b2364c
demattia <marco.de.mattia@cern.ch>
2016-03-18 09:27:24 -0500

    Reverted back to FileInPath and made it work

--- AMTrackProducer/python/AMtracks_cfi.py
+++ AMTrackProducer/python/AMtracks_cfi.py
@@ -2,8 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 AMTrackProducer = cms.EDProducer('AMTrackProducer',
     inputTagStub=cms.InputTag('TTStubsFromPixelDigis', 'StubAccepted'),
-    RoadsInputTag=cms.InputTag("AMRoadProducer", "Level1Roads"),
-    ConstantsDir=cms.string("/fdata/hepx/store/user/rish/AMSIMULATION/Forked/CMSSW_6_2_0_SLHC25_patch3/src/AMCMSSWInterface/AMTrackProducer/python/ConstantsProduction/"),
+    # RoadsInputTag=cms.InputTag("AMRoadProducer", "Level1Roads"),
+    RoadsInputTag= cms.InputTag("MergeTCOutput", "AML1TCs"),
+    ConstantsDir=cms.FileInPath("AMCMSSWInterface/AMTrackProducer/python/ConstantsProduction/PreEstimate_Transverse/matrixVD_2016.txt"),
     outputCommands = cms.untracked.vstring(
         "drop *",
         "keep *_ntuple*_*_*",
